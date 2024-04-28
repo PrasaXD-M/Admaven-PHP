84bad6616c29bae656718e268c26992d571fa090
Prasad Manamperi <166655653+PrasaXD-M@users.noreply.github.com>
2024-04-28 05:38:54 +0530

    updated

--- signUp.php
+++ signUp.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,76 +8,79 @@
     <link rel="stylesheet" href="signUp.css">
 </head>
 <body>
-    <div class="signup-container">
 
-        <?php
-            // print_r($_POST);
-            if(isset($_POST["submit"])) {
-                $fname = $_POST["rfName"];
-                $email = $_POST["uemail"];
-                $password = $_POST["email"];
-                $passwordRepeat = $_POST["rePassword"];
-    
-                $errors = array();
+    <div class="form-body">
+        <div class="signup-container">
 
-                if(empty($fname) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+            <?php
+                // print_r($_POST);
+                if(isset($_POST["submit"])) {
+                    $fname = $_POST["rfName"];
+                    $email = $_POST["uemail"];
+                    $password = $_POST["password"];
+                    $passwordRepeat = $_POST["rePassword"];
+        
+                    $errors = array();
 
-                    array_push($errors, "All fields are required");
-                }
-
-                if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    array_push($errors, "Email is not Valid!");
-                }
-
-                if(strlen($password) < 4) {
-                    array_push($errors, "Password should be contain 6 characters!");
-                }
-
-                if($password !== $passwordRepeat) {
-                    array_push($errors, "Password does not match");
-                }
-
-                if(count($errors) > 0) {
-                    foreach($errors as $error) {
-                        echo "<div class = 'error-alert'>$error</div>";
+                    if(empty($fname) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+                        array_push($errors, "All fields are requied!");
                     }
 
-                } else {
-                    //insert data into the database.
+                    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        array_push($errors, "Email is not Valid!");
+                    }
+
+                    if(strlen($password) < 4) {
+                        array_push($errors, "Password should be contain 6 characters!");
+                    }
+
+                    if($password !== $passwordRepeat) {
+                        array_push($errors, "Password does not match");
+                    }
+
+                    if(count($errors) > 0) {
+                        foreach($errors as $error) {
+                            echo "<div class = 'error-alert'>$error</div>";
+                        }
+
+                    } else {
+                        //insert data into the database.
+                    }
                 }
-            }
-        ?>
+            ?>
 
-        <form action="signUp.php" method="post">
-            <div class="form-bdy">
-                <label for="rfName">First Name:</label>
-                <input type="text" name="rfName" id="rfName" placeholder="Your fist name..">
-            </div>
-            <!-- <div class="form-bdy">
-                <label for="rlName">Last Name:</label>
-                <input type="text" name="rlName" id="rlName" placeholder="Your last name..">
-            </div> -->
-            <div class="form-bdy">
-                <label for="uemail">Email:</label>
-                <input type="email" name="uemail" id="uemail" placeholder="eg: 123@gmail.com">
-            </div>
-            <div class="form-bdy">
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password">
-            </div>
-            <!-- <div class="form-bdy">
-                <label for="cont-num">Contact No:</label>
-                <input type="tel" name="cont-num" id="cont-num">
-            </div> -->
-            <div class="form-bdy">
-                <label for="rePassword">Repeat Password:</label>
-                <input type="password" name="rePassword" id="rePassword">
-            </div>
-            <div class="form-bdy">
-                <input type="submit" value="Register" name="submit">
-            </div>
-        </form>
+            <form action="signUp.php" method="post">
+                <div class="form-bdy">
+                    <label for="rfName">First Name:</label>
+                    <input type="text" name="rfName" id="rfName" placeholder="Your fist name..">
+                </div>
+                <!-- <div class="form-bdy">
+                    <label for="rlName">Last Name:</label>
+                    <input type="text" name="rlName" id="rlName" placeholder="Your last name..">
+                </div> -->
+                <div class="form-bdy">
+                    <label for="uemail">Email:</label>
+                    <input type="email" name="uemail" id="uemail" placeholder="eg: 123@gmail.com">
+                </div>
+                <div class="form-bdy">
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password">
+                </div>
+                <!-- <div class="form-bdy">
+                    <label for="cont-num">Contact No:</label>
+                    <input type="tel" name="cont-num" id="cont-num">
+                </div> -->
+                <div class="form-bdy">
+                    <label for="rePassword">Repeat Password:</label>
+                    <input type="password" name="rePassword" id="rePassword">
+                </div>
+                <div class="form-bdy">
+                    <input type="submit" value="Register" name="submit">
+                </div>
+            </form>
 
+        </div>
     </div>
+
 </body>
 </html>
