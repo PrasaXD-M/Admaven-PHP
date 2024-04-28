2f11149c939c262f474bd81ef219f6127c5cc3de
Prasad Manamperi <166655653+PrasaXD-M@users.noreply.github.com>
2024-04-28 05:53:04 +0530

    created a connection with the database

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php 
+    $hostName = "localhost";
+    $dbUser = "root";
+    $dbpassowrd = "";
+    $dbName = "testlogin";
+
+    $conn = mysqli_connect($hostName, $dbUser, $dbpassowrd, $dbName);
+?>
