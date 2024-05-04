3b9ed06420547030db9d404a4444b75641c8ff0a
Prasad Manamperi <166655653+PrasaXD-M@users.noreply.github.com>
2024-05-04 12:56:59 +0530

    updated

--- editDetails.php
+++ editDetails.php
@@ -1,358 +1,146 @@
-<?php 
-    // session_start();
-    if(isset($_SESSION['user'])) {
 
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<style>
-    input[type=text], input[type=password] {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        box-sizing: border-box;
-    }
-    .edit__btn {
-        background-color: #04AA6D;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-    }
-
-    .edit__btn:hover {
-        opacity: 0.8;
-    }
-
-    /* Extra styles for the cancel button */
-    .edit__cancelbtn {
-        width: auto;
-        padding: 10px 18px;
-        background-color: #f44336;
-    }
-
-    /* Center the image and position the close button */
-    .imgcontainer {
-        text-align: center;
-        margin: 24px 0 12px 0;
-        position: relative;
-    }
-
-    img.user_prof {
-        width: 40%;
-        border-radius: 50%;
-    }
-
-    .container {
-        padding: 16px;
-    }
-
-    span.psw {
-        float: right;
-        padding-top: 16px;
-    }
-
-    /* The Modal (background) */
-    .modal {
-        display: none; /* Hidden by default */
-        position: fixed; /* Stay in place */
-        z-index: 1; /* Sit on top */
-        left: 0;
-        top: 0;
-        width: 100%; /* Full width */
-        height: 100%; /* Full height */
-        overflow: auto; /* Enable scroll if needed */
-        background-color: rgb(0,0,0); /* Fallback color */
-        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-        padding-top: 60px;
-    }
-
-    /* Modal Content/Box */
-    .modal-content {
-        background-color: #fefefe;
-        margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-        border: 1px solid #888;
-        width: 80%; /* Could be more or less, depending on screen size */
-    }
-
-    /* The Close Button (x) */
-    .close {
-        position: absolute;
-        right: 25px;
-        top: 0;
-        color: #000;
-        font-size: 35px;
-        font-weight: bold;
-    }
-
-    .close:hover,
-    .close:focus {
-        color: red;
-        cursor: pointer;
-    }
-
-    .animate {
-        -webkit-animation: animatezoom 0.6s;
-        animation: animatezoom 0.6s
-    }
-
-    @-webkit-keyframes animatezoom {
-        from {-webkit-transform: scale(0)} 
-        to {-webkit-transform: scale(1)}
-    }
-    
-    @keyframes animatezoom {
-        from {transform: scale(0)} 
-        to {transform: scale(1)}
-    }
-
-    @media screen and (max-width: 300px) {
-        span.psw {
-            display: block;
-            float: none;
+    <title>Sign Up to AdMaven</title>
+    <link rel="stylesheet" href="editpage.css">
+    <style>
+        @media screen and (max-width:700px){
+            .button-flex {
+                flex-direction: column;
+            }
         }
-        .cancelbtn {
-            width: 100%;
+
+        @media screen and (min-width: 750px){
+            .signup-container {
+                width: 40em;
+            }
         }
-    }
-</style>
+
+        body{
+            background-image: url("https://th.bing.com/th/id/R.0e9ed103714e886da56317a8adcc7c4e?rik=XA5cl9krLY8U8Q&riu=http%3a%2f%2fcdn.wallpapersafari.com%2f80%2f28%2fLbzN9a.jpg&ehk=b%2fMaBFPVkyKmFbvlBc8z%2fOP9pYWYu6djADuUSKRbr9g%3d&risl=&pid=ImgRaw&r=0");
+        }
+
+        .signup-container {
+            /* background-image: url("img/generic-background.jpg"); */
+        }
+
+
+    </style>
 </head>
 <body>
 
-<button class="edit__btn" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Edit Details</button>
+    <div class="form-body">
+        <div class="signup-container">
 
-<div id="id01" class="modal">
-  
-  <form class="modal-content animate" action="/action_page.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img/homeimg/image4.jpg" alt="userProfile" class="user_prof">
-    </div>
+            <div class="signup-head">
+                <h2>Welcome To AdMaven</h2>
+            </div>
 
-    <div class="container">
-      <label for="fname"><b>New First Name:</b></label>
-      <input type="text" placeholder="Enter first name" name="fname" required>
-      <label for="lname"><b>New First Name:</b></label>
-      <input type="text" placeholder="Enter Last name" name="lname" required>
-
-      <label for="psw"><b>New Password:</b></label>
-      <input type="password" placeholder="Enter Password" name="npassword" required>
+            <?php
+                // print_r($_POST);
+                if(isset($_POST["submit"])) {
+                    $fname = $_POST["rfName"];
+                    $lname = $_POST["rlName"];
+                    $email = $_POST["uemail"];
+                    $password = $_POST["password"];
+                    $passwordRepeat = $_POST["rePassword"];
         
-      <input type="submit" class="edit__btn" name="update" value="Update">
+                    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+                    $errors = array(); 
+
+                    if(empty($fname) OR empty($lname) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+                        array_push($errors, "All fields are requied!");
+                    }
+
+                    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        array_push($errors, "Email is not Valid!");
+                    }
+
+                    if(strlen($password) < 4) {
+                        array_push($errors, "Password should be contain 6 characters!");
+                    }
+
+                    if($password !== $passwordRepeat) {
+                        array_push($errors, "Password does not match");
+                    }
+
+                    require_once "config/database.php";
+
+                    // $sql = "SELECT * FROM  WHERE email = '$email'";
+                    // $result = mysqli_query($con, $sql);
+
+                    // $rowCount = mysqli_num_rows($result);
+
+                    if(count($errors) > 0) {
+                        foreach($errors as $error) {
+                            echo "<div class = 'error-alert'>$error</div>";
+                        }
+
+                    } else {
+                        mysqli_query($con, "INSERT INTO user_registration (name, L_name,  email, password) VALUES ('$fname','$lname', '$email', '$password')");
+
+                        echo "<div class = 'success-alert'>Welcome to AdMaven You are registered successfully!</div>";
+                        header("location: login.php");
+                    }
+                }
+            ?>
+
+            <?php 
+                if(isset($_POST["cancel"])) {
+                    header("location: /index.php");
+                } 
+            ?>
+
+            <form action="editDetails.php" method="post">
+                <div class="form-bdy">
+                    <label for="rfName">First Name:</label><br>
+                    <input type="text" name="rfName" id="rfName" placeholder="Your fist name.." style="width: 100%;">
+                </div>
+                <div class="form-bdy">
+                    <label for="rlName">Last Name:</label><br>
+                    <input type="text" name="rlName" id="rlName" placeholder="Your last name.." style="width: 100%;">
+                </div>
+                <div class="form-bdy">
+                    <label for="uemail">Email:</label><br>
+                    <input type="email" name="uemail" id="uemail" placeholder="eg: 123@gmail.com" style="width: 100%;">
+                </div>
+                <div class="form-bdy">
+                    <label for="password">Password:</label><br>
+                    <input type="password" name="password" id="password" style="width: 100%;">
+                </div>
+                <!-- <div class="form-bdy">
+                    <label for="cont-num">Contact No:</label><br>
+                    <input type="tel" name="cont-num" id="cont-num" style="width: 100%;">
+                </div> -->
+                <div class="form-bdy">
+                    <label for="rePassword">Repeat Password:</label><br>
+                    <input type="password" name="rePassword" id="rePassword" style="width: 100%;">
+                </div>
+
+                <!-- <div class="already-msg">
+                    <p>Already have an account? <a href="login.php">Login here</a></p>
+                </div> -->
+
+                <div class="form-bdy button-flex" style="width: 100%;">
+                    <div class="reg">
+                        <input type="submit"value="Update" 
+                        name="submit">
+                    </div>
+                    <div class="back">
+                        <input type="submit" value="Cancel"
+                        name="cancel">
+                    </div>
+                </div>
+            </form>
+            <div class="remember-terms">
+                <p>By continuing, you agree to AdMaven's <a href="#">Tearms of service & privacy policy.</a></p>
+            </div>
+        </div>
     </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="edit__cancelbtn">Cancel</button>
-    </div>
-  </form>
-</div>
-
-<script>
-
-var modal = document.getElementById('id01');
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
 
 </body>
 </html>
-<?php } elseif(isset($_SESSION['consultant'])) { ?>
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<style>
-    input[type=text], input[type=password], input[type=email] {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        box-sizing: border-box;
-    }
-    .edit__btn {
-        background-color: #04AA6D;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-    }
-
-    .edit__btn:hover {
-        opacity: 0.8;
-    }
-
-    /* Extra styles for the cancel button */
-    .edit__cancelbtn {
-        width: auto;
-        padding: 10px 18px;
-        background-color: #f44336;
-    }
-
-    /* Center the image and position the close button */
-    .imgcontainer {
-        text-align: center;
-        margin: 24px 0 12px 0;
-        position: relative;
-    }
-
-    img.user_prof {
-        width: 40%;
-        border-radius: 50%;
-    }
-
-    .container {
-        padding: 16px;
-    }
-
-    span.psw {
-        float: right;
-        padding-top: 16px;
-    }
-
-    /* The Modal (background) */
-    .modal {
-        display: none; /* Hidden by default */
-        position: fixed; /* Stay in place */
-        z-index: 1; /* Sit on top */
-        left: 0;
-        top: 0;
-        width: 100%; /* Full width */
-        height: 100%; /* Full height */
-        overflow: auto; /* Enable scroll if needed */
-        background-color: rgb(0,0,0); /* Fallback color */
-        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-        padding-top: 60px;
-    }
-
-    /* Modal Content/Box */
-    .modal-content {
-        background-color: #fefefe;
-        margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-        border: 1px solid #888;
-        width: 80%; /* Could be more or less, depending on screen size */
-    }
-
-    /* The Close Button (x) */
-    .close {
-        position: absolute;
-        right: 25px;
-        top: 0;
-        color: #000;
-        font-size: 35px;
-        font-weight: bold;
-    }
-
-    .close:hover,
-    .close:focus {
-        color: red;
-        cursor: pointer;
-    }
-
-    .animate {
-        -webkit-animation: animatezoom 0.6s;
-        animation: animatezoom 0.6s
-    }
-
-    @-webkit-keyframes animatezoom {
-        from {-webkit-transform: scale(0)} 
-        to {-webkit-transform: scale(1)}
-    }
-    
-    @keyframes animatezoom {
-        from {transform: scale(0)} 
-        to {transform: scale(1)}
-    }
-
-    @media screen and (max-width: 300px) {
-        span.psw {
-            display: block;
-            float: none;
-        }
-        .cancelbtn {
-            width: 100%;
-        }
-    }
-</style>
-</head>
-<body>
-
-<button class="edit__btn" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Edit Details</button>
-
-<div id="id01" class="modal">
-
-    <?php 
-        require 'config/database.php';
-
-        
-    ?>
-
-  <form class="modal-content animate" action="editDetails.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img/homeimg/image4.jpg" alt="userProfile" class="user_prof"> 
-    </div>
-
-    <div class="container">
-      <label for="fname"><b>First Name:</b></label>
-      <input type="text" placeholder="Enter first name" name="fname" required><br>
-
-      <label for="lname"><b>Last Name:</b></label>
-      <input type="text" placeholder="Enter Last name" name="lname" required><br>
-
-      <label for="email"><b>Email:</b></label>
-      <input type="email" placeholder="Enter your email" name="email" required><br>
-
-      <label for="contactNo"><b>Contact Number:</b></label>
-      <input type="text" placeholder="Enter Contact number" name="contactNo" required>
-
-      <label for="npassword"><b>New Password:</b></label>
-      <input type="password" placeholder="Enter Password" name="npassword" required><br>
-
-      <label for="constimg"><b>Profile photo:</b></label>
-      <input type="file" name="constimg" id="constimg" accept="image/png, image/gif, image/jpeg">
-        
-      <input type="submit" class="edit__btn" name="update" value="Update">
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="edit__cancelbtn">Cancel</button>
-    </div>
-  </form>
-</div>
-
-<script>
-
-var modal = document.getElementById('id01');
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-
-</body>
-</html>
-  
-<?php } ?>
