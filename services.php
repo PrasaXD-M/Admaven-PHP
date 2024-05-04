2115884fb7978b78e49abb26991435465481a058
Prasad Manamperi <166655653+PrasaXD-M@users.noreply.github.com>
2024-05-04 20:00:48 +0530

    create css file for service

--- services.php
+++ services.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/services.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="Service">
+        <?php include ("header.php"); ?>
+    </div>
+
+    <div class="service-header-container">
+        
+    </div>
 </body>
 </html>
