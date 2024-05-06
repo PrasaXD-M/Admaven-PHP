441983065da383d4e43b70ea41407d125a6b2038
Prasad Manamperi <166655653+PrasaXD-M@users.noreply.github.com>
2024-05-07 03:56:32 +0530

    added testtimonial folder

--- index.php
+++ index.php
@@ -105,7 +105,7 @@
     </div>
 
     <div class="testimonial item5">
-        <?php include "Testimonial/testimonial.php"; ?> 
+        <?php //include "Testimonial/testimonial.php"; ?> 
 
     </div>
 
